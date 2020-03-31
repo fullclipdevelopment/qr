043c6cba68a48f26ee827e08dec69b13b2fc1c96
Hosea Sandstrom <24390178+hsandstromOM@users.noreply.github.com>
2020-03-31 11:47:29 -0400

    update angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatBadgeModule
-} from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreateQrComponent } from './create-qr/create-qr.component';
